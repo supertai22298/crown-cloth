6f76c446f7f04fd2a644b7fdc24cadfd56339863
Nguyen Van Tai <supertai22298@gmail.com>
2020-07-14 13:09:21 +0700

    sign in component and reusable input form component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './sign-in-and-sign-up.style.scss'
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+const SignInAndSignUp = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  )
+}
+
+export default SignInAndSignUp
